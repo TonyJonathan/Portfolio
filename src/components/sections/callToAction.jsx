2c52644190b5984125bc52d0943a7de79b928b0e
tony <tony.bruchon@hotmail.fr>
2025-01-26 23:58:56 +0100

    Update project API photo

--- src/components/sections/callToAction.jsx
+++ src/components/sections/callToAction.jsx
@@ -2,24 +2,30 @@ import React from 'react'
 import { Link } from 'react-router-dom'
 import { RiDownloadLine } from '@remixicon/react'
 import SlideUp from '../../utlits/animations/slideUp'
+import { useLocation } from 'react-router-dom';
 
 const CallToAction = () => {
+
+    const location = useLocation(); 
+
     return (
         <section className="call-to-action-area">
             <div className="container">
                 <div className="row">
                     {/* <!-- START ABOUT TEXT DESIGN AREA --> */}
-                    <div className="col-lg-12">
-                        <SlideUp>
-                            <div className="about-content-part call-to-action-part text-center">
-                                <h2>Are You Ready to kickstart your project with a touch of magic?</h2>
-                                <p>Reach out and let's make it happen ✨. I am available for an apprenticeship, whether on-site or remotely, to deliver exceptional work and actively contribute to your team!</p>
-                                <div className="hero-btns">
-                                    <Link to="/contact" className="theme-btn">Let's Talk  <i><RiDownloadLine size={16} /></i></Link>
+                    {location.pathname.endsWith('/contact') ? null : (
+                        <div className="col-lg-12">
+                            <SlideUp>
+                                <div className="about-content-part call-to-action-part text-center">
+                                    <h2>Are You Ready to kickstart your project with a touch of magic?</h2>
+                                    <p>Reach out and let's make it happen ✨. I am available for an apprenticeship, whether on-site or remotely, to deliver exceptional work and actively contribute to your team!</p>
+                                    <div className="hero-btns">
+                                        <Link to="/contact" className="theme-btn">Let's Talk  <i><RiDownloadLine size={16} /></i></Link>
+                                    </div>
                                 </div>
-                            </div>
-                        </SlideUp>
-                    </div>
+                            </SlideUp>
+                        </div>
+                    )}
                     {/* <!-- / END ABOUT TEXT DESIGN AREA --> */}
                 </div>
             </div>
