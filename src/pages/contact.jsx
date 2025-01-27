3c3b52f38ce455496c81ae678900eee49d152db3
tony <tony.bruchon@hotmail.fr>
2025-01-27 23:57:13 +0100

    Update page title

--- src/pages/contact.jsx
+++ src/pages/contact.jsx
@@ -10,7 +10,6 @@ const Contact = () => {
           <div class="row">
             <div class="col-xl-12 col-lg-12">
               <div class="section-title text-center wow fadeInUp delay-0-2s">
-                <p>contact</p>
                 <h2>Get in Touch with Me!</h2>
               </div>
             </div>
